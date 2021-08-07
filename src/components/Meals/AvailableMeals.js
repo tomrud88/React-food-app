4ee7904487b68c89dd288fe2d2e4707046489881
Tomasz Rudnicki <tomek198821@wp.pl>
2021-08-07 15:57:42 +0100

    practice

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,48 +5,45 @@ import { useState, useEffect } from 'react';
 
 
   const AvailableMeals = () => {
+
     const[mealsList,setMealsList] = useState([]);
-    const[isLoading,setIsLoading] = useState(true);
-    const[httpError,setHttpError] = useState()
+    const[loading,setLoading] = useState(true);
+    const[error,setError] = useState()
 
     useEffect(() => {
-      const fetchList = async()=>{
-        const response = await fetch('https://food-order-app-cf489-default-rtdb.firebaseio.com/meals.json');
-       
-        if(!response.ok){
-          throw new Error('something went wrong')
-        }
-  
-        const data = await response.json();
-  
-        console.log(data)
-        let loadingArray = []
-  
-        for(const key in data){
-           loadingArray.push(
-             {
-               id:key,
-               description: data[key].description,
-               name:data[key].name,
-               price:data[key].price
-             }
-           )
-        }
-        console.log(loadingArray)
-        setMealsList(loadingArray)
-        setIsLoading(false)
-      }
-  
-      fetchList().catch((error)=>{
-         setHttpError(error.message);
-         setIsLoading(false)
-      })
-    }, [])
+     const fetchList = async() =>{
+      const response = await fetch('https://food-order-app-cf489-default-rtdb.firebaseio.com/meals.json');
+      
 
-    
-     let errorMessage = httpError && <section><p className={classes.error}>{httpError}</p></section>
-     let loading = isLoading && <section><p className={classes.mealLoading}>Loading...</p></section>
-     
+      if(!response.ok){
+        throw new Error('something went wrong')
+      }
+
+      const data = await response.json();
+
+      let loadedMeals = [];
+
+      for(const key in data){
+        loadedMeals.push(
+        {
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price
+        })
+      }
+        setMealsList(loadedMeals)
+        setLoading(false)
+      }
+        fetchList().catch(error =>{
+           setError(error.message);
+           setLoading(false)
+        })
+    },[])
+  
+     const load = loading && <h1 className={classes.mealLoading}>Loading...</h1>;
+     const errorMessage = error && <p className={classes.error}>{error}</p>
+
      const mealList = mealsList.map(meal => <MealItem 
       id={meal.id}
       key={meal.id}
@@ -60,7 +57,7 @@ import { useState, useEffect } from 'react';
         <section className={classes.meals}>
           <Card>
             {errorMessage}
-            {loading}
+            {load}
             <ul>{mealList}</ul>
           </Card>
         </section>
